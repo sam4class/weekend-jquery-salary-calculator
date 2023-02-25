d05ef40544a200169dc6cc89e06eb9b100113cdc
Samantha Marlowe <sam.c.marlowe@gmail.com>
2023-02-25 16:41:34 -0600

    seeing if I can find this commit on the new repo

---
+++ salaryCal.js
@@ -0,0 +1,135 @@
+console.log('Here We Go . . .');
+
+let employee = [];
+let budget = [];
+
+$(document).ready(onReady);
+
+function onReady() {
+    //are we in?
+    console.log('DOM is loaded');
+    //create an action when submit button is pressed
+    $('#submitNewEmployeeBtn').on('click', addNewEmployeeInfo);
+    //Listner for the budget updates
+    let nowBudget = $('#budgetNeeded');
+    nowBudget.empty();
+    nowBudget.append(budget);
+    //listener for delete button- we can do this!
+    $('#addedEmployee').on('click', '#deleteBtn', deleteEmployee);
+
+}
+
+function deleteEmployee() {
+    console.log('Inside deleteEmployee()');
+
+    let newEmployee = [];
+    let employeeToDeleteName = $(this).parent().siblings().first().text();
+    console.log('Name to delete:', employeeToDeleteName);
+
+    for (let person of employee) {
+        if (person['First Name'] !== employeeToDeleteName) {
+            newEmployee.push(person);
+        }
+    }
+    employee = newEmployee;
+    render();
+}
+
+function addNewEmployeeInfo() {
+    //are we in?
+    console.log('Inside addNewEmployeeInfo');
+
+    //add values to each input box
+    const firstNameInputValue = $('#firstNameInput').val();
+    const lastNameInputValue = $('#lastNameInput').val();
+    const idNumberInputValue = $('#idNumberInput').val();
+    const jobTitleInputValue = $('#jobTitleInput').val();
+    const annualSalaryInputValue = $('#annualSalaryInput').val();
+    //making sure the values were added
+    console.log(`Input Values:
+        ${firstNameInputValue},
+        ${lastNameInputValue},
+        ${idNumberInputValue},
+        ${jobTitleInputValue},
+        ${annualSalaryInputValue}`);
+
+    //Create an object to hold the inputed values
+
+    const newEmployeeAdd = {
+        'First Name': firstNameInputValue,
+        'Last Name': lastNameInputValue,
+        'ID Number': idNumberInputValue,
+        'Job Title': jobTitleInputValue,
+        'Annual Salary': annualSalaryInputValue
+    }
+
+    //Add to array
+    employee.push(newEmployeeAdd);
+    console.log('List with added employee', employee);
+
+    //calculate addingbudget
+    addingBudget();
+
+    //add render here
+    render();
+    //place function that clears feilds here
+    resetForm();
+}
+
+//Create a function that clears the input fields
+function resetForm() {
+    console.log("Inside of resetForm()");
+    // Setter: making the .val a setter not a getter
+    $('#firstNameInput').val('');
+    $('#lastNameInput').val('');
+    $('#idNumberInput').val('');
+    $('#jobTitleInput').val('');
+    $('#annualSalaryInput').val('');
+}
+
+function addingBudget() {
+    //are you in?
+    console.log('Inside addingBudget');
+    let totalCost = 0;
+    console.log(employee);
+    //loop through each salary and grab the annual salary
+    for (let i = 0; i < employee.length; i++) {
+        totalCost += Number(employee[i]['Annual Salary']);
+        //    console.log(employee[i].annualSalaryInputValue);
+    }
+    console.log('totalCost:', totalCost);
+    //find the monthly budget by dividing the annual by 12 and epmty that value to get the next
+    let monthlyCost = totalCost / 12;
+    let nowBudget = $('#budgetNeeded');
+    nowBudget.empty();
+    nowBudget.append(monthlyCost);
+
+    /*If the total monthly cost exceeds $20,000, add a 
+    red background color to the total monthly cost.*/
+
+    if (monthlyCost >= 20000) {
+        console.log('Current total budget', monthlyCost);
+        $('#budgetNeeded').css('background-color', 'red');
+    }
+}
+
+
+function render() {
+    //am i here?
+    console.log('Render that!')
+    //reset the employee form
+    $('#addedEmployee').empty();
+    //Loop over the employee list and add the new input of each value
+    for (person of employee) {
+        $('#addedEmployee').append(`   
+    <tr>
+        <td>${person['First Name']}</td> 
+        <td>${person['Last Name']}</td>
+        <td>${person['ID Number']}</td>
+        <td>${person['Job Title']}</td> 
+        <td>${person['Annual Salary']}</td> 
+        <td><button id="deleteBtn">Delete</button></td>
+    </tr>
+    `);
+    }
+}
